72c77f8c77aec4f66fb550d32b6f0e1cb513517d
Amri <57156706+FamryAmri@users.noreply.github.com>
2024-01-29 08:53:35 +0000

    Add files via upload

--- routers/database.js
+++ routers/database.js
@@ -7,6 +7,7 @@ const social = require ('./parts/social');
 const posts = require ('./parts/posts');
 const misc = require ('./parts/misc');
 
+app.post("/getGJUsers:v.php", user.getusersearch);
 app.post ("/getGJUserList:v.php", user.getuserlist);
 app.post("/getGJUserInfo:v.php", user.getuser);
 app.post("/updateGJUserScore:v.php", user.updateuser);
@@ -22,6 +23,10 @@ app.post("/uploadGJLevel:v.php", levels.uploads);
 app.post("/getGJLevels:v.php", levels.getlevels);
 app.post("/downloadGJLevel:v.php", levels.download);
 app.post('/deleteGJLevelUser:v.php', levels.deletelevel);
+app.post("/suggestGJStars:v.php", levels.ratingLevel);
+app.post("/rateGJDemon:v.php", levels.rateDemonLevel);
+
+app.post('/getGJDailyLevel.php', levels.getDaily);
 
 app.post("/acceptGJFriendRequest:v.php", social.acceptReq);
 app.post ("/uploadFriendRequest:v.php", social.addreq);
@@ -37,6 +42,11 @@ app.post("/uploadGJMessage:v.php", social.sentmessage);
 app.post("/downloadGJMessage:v.php", social.readmessage);
 app.post("/deleteGJMessages:v.php", social.removemessage);
 
+app.post("/uploadGJComment:v.php", social.sentcomment);
+app.post("/getGJComments:v.php", social.getcomment);
+app.post("/getGJCommentHistory.php", social.getcomment);
+
+app.post("/requestUserAccess.php", misc.getmod);
 app.post ('/getGJRewards.php', misc.getchest);
 app.post ('/getGJChallenges.php', misc.getquest);
 app.post ("/getGJSongInfo.php", misc.getsong);
